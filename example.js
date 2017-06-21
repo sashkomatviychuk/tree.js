c3c6dff75272798805eeb221a75ff225354c1029
Sashko Matviychuk <sashko1921@gmail.com>
2017-06-21 21:57:05 +0200

    Fix tabs.

--- example.js
+++ example.js
@@ -1,26 +1,26 @@
 const makeTree = require('./tree');
 const list = [
-		{
-			_id: 1,
-			_parent: null,
-			text: '123',
-		},
-		{
-			_id: 2,
-			_parent: 1,
-			text: '22222',
-		},
-		{
-			_id: 3,
-			_parent: 2,
-			text: '333333',
-		},
-		{
-			_id: 4,
-			_parent: null,
-			text: '123 4',
-		},
-	];
+	{
+		_id: 1,
+		_parent: null,
+		text: '123',
+	},
+	{
+		_id: 2,
+		_parent: 1,
+		text: '22222',
+	},
+	{
+		_id: 3,
+		_parent: 2,
+		text: '333333',
+	},
+	{
+		_id: 4,
+		_parent: null,
+		text: '123 4',
+	},
+];
 
 const tree = makeTree(list)
 
